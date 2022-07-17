b353ceb963204487d62b4fa853bfd5b3ef503d1a
Hephaestus <mcmahonmike9@yahoo.com>
2022-07-17 16:16:26 -0500

    nav bar moved into header, project folder built

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className='App'>
       <Header/>
-      <Nav></Nav>
       <main>
       </main>
       <Footer/>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <section>
+        <footer>
+         < FaGithub />
+         < FaLinkedin />
+         < FaStackOverflow />
 
-        </section>
+        </footer>
     )
 }
 
