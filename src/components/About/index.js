7118ac122fb995782cfa33266793f2a1ec14382d
Hephaestus <mcmahonmike9@yahoo.com>
2022-07-21 17:43:13 -0500

    adjusting styles and portfolio/project components

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,13 @@ import React from "react";
 
 function About() {
     return (
-            <body>
+            <section className="my-5">
+              <div>
               <h1> About Me</h1>
               <p>My name is Michael McMahon and I am a developer in training </p>
-            </body>
+              </div>
+              </section>
+
           );
 }
 
